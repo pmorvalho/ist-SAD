eb3b3b0a8e9cc55c8d1ec20b7af7fe7b729e6835
Daniel Correia <dduarte96@hotmail.com>
2017-10-22 21:52:22 +0100

    updated noshows scripts and generated clusters by days

--- noshows/preprocess_noshows.py
+++ noshows/preprocess_noshows.py
@@ -2,14 +2,14 @@ import pandas as pd
 import datetime
 
 dataset = pd.read_csv("data/noshows.csv")
-print len(dataset)
+# print len(dataset)
 dataset_sample = dataset.sample(frac=0.1, replace=True)
-print len(dataset_sample)
+# print len(dataset_sample)
 # convert M e F para numeros
 dataset['Gender'] = dataset['Gender'].replace('F',1)
 dataset['Gender'] = dataset['Gender'].replace('M',0)
 
-print "Ja tratou dos sexos"
+# print "Ja tratou dos sexos"
 # drop da coluna sp porque estamos a trabalhar em clustering
 dataset.drop('No-show',1,inplace=True)
 
@@ -26,34 +26,29 @@ dic= dict(dic)
 for d in dic:
  	dataset['Neighbourhood'] = dataset['Neighbourhood'].replace(d,dic[d])
 
+dataset.AppointmentDay = pd.to_datetime(pd.Series(dataset.AppointmentDay))
+dataset.AppointmentDay = dataset.AppointmentDay.dt.day
+
+dataset.ScheduledDay = pd.to_datetime(pd.Series(dataset.ScheduledDay))
+dataset.ScheduledDay = dataset.ScheduledDay.dt.day
+
+
 # print "Ja tratou das Localidades"
-
-dic = []
-for d in dataset['ScheduledDay']:
-	date = datetime.datetime.strptime( d, "%Y-%m-%dT%H:%M:%SZ" )
-	dic.append((d, datetime.datetime.strftime(date,'%W')))
-
-print 
-dic = dict(dic)
-for d in dic:
-	dataset['ScheduledDay'] = dataset['ScheduledDay'].replace(d,dic[d])
-
-print "Ja tratou dos ScheduledDay"
-dic = []
-for d in dataset['AppointmentDay']:
-	date = datetime.datetime.strptime( d, "%Y-%m-%dT%H:%M:%SZ" )
-	dic.append((d, datetime.datetime.strftime(date,'%W')))
-
-dic = dict(dic)
-for d in dic:
-	dataset['AppointmentDay'] = dataset['AppointmentDay'].replace(d,dic[d])
-
-print "Ja tratou dos AppointmentDay"
 
 #  drop PatientId,AppointmentID
 
 # ficheiro base com tudo em numeros
 dataset.to_csv("data/base_noshows.csv", index=False)
+
+# dataset.drop('PatientId',1,inplace=True)
+dataset.drop('AppointmentID',1,inplace=True)
+
+dataset.to_csv("data/base_kmeans_noshows.csv", index=False)
+
+for i in range(10):
+	sample = dataset.sample(frac=0.1,replace=False)
+	filename = "data/base_kmeans_sample_" + str(i) + ".csv"
+	sample.to_csv(filename, index=False)
 
 
 # # Primeira tentativa: truncar os floats para int a bruta
