e32145137d87635b4d97a136bb94d50252d58d6f
Pedro Miguel Orvalho Marques da Silva <ist181151@sigma00.ist.utl.pt>
2017-10-22 20:20:09 +0100

    base_noshows with days

--- noshows/preprocess_noshows.py
+++ noshows/preprocess_noshows.py
@@ -2,62 +2,51 @@ import pandas as pd
 import datetime
 
 dataset = pd.read_csv("data/noshows.csv")
-print len(dataset)
-dataset_sample = dataset.sample(frac=0.1, replace=True)
-print len(dataset_sample)
+
 # convert M e F para numeros
 dataset['Gender'] = dataset['Gender'].replace('F',1)
 dataset['Gender'] = dataset['Gender'].replace('M',0)
 
-print "Ja tratou dos sexos"
 # drop da coluna sp porque estamos a trabalhar em clustering
 dataset.drop('No-show',1,inplace=True)
 
+# di fica com as varias localidades unicas do dataset
 di = set(dataset['Neighbourhood'])
 
 dic = []
 i=1
+# para cada uma das localidades (d) vamos atribuir um valor numerico (i) e colocar numa lista (dic)
 for d in di:
 	dic.append((d, i))
 	i+=1
 
+#fazemos da lista dic um dicionario
 dic= dict(dic)
 
+#alteramos cada uma das localicadas pelo respectivo valor numerico do dicionario
 for d in dic:
  	dataset['Neighbourhood'] = dataset['Neighbourhood'].replace(d,dic[d])
 
-# print "Ja tratou das Localidades"
-
+#para cada data (d) pomos na lista dic um tuplo da data com o respectivo numero da semana do ano da data
 dic = []
 for d in dataset['ScheduledDay']:
 	date = datetime.datetime.strptime( d, "%Y-%m-%dT%H:%M:%SZ" )
-	dic.append((d, datetime.datetime.strftime(date,'%W')))
+	dic.append((d, date.timetuple().tm_yday))
 
-print 
+# fazemos um dicionario da lista e fazemos uma alteracao no dataset cada data passa a ser o numero da semana respectiva nesse ano
 dic = dict(dic)
 for d in dic:
 	dataset['ScheduledDay'] = dataset['ScheduledDay'].replace(d,dic[d])
 
-print "Ja tratou dos ScheduledDay"
+#repetimos o que fizemos para o ScheduleDay para o AppointmentDay
 dic = []
 for d in dataset['AppointmentDay']:
 	date = datetime.datetime.strptime( d, "%Y-%m-%dT%H:%M:%SZ" )
-	dic.append((d, datetime.datetime.strftime(date,'%W')))
+	dic.append((d, date.timetuple().tm_yday))
 
 dic = dict(dic)
 for d in dic:
 	dataset['AppointmentDay'] = dataset['AppointmentDay'].replace(d,dic[d])
 
-print "Ja tratou dos AppointmentDay"
-
-#  drop PatientId,AppointmentID
-
 # ficheiro base com tudo em numeros
-dataset.to_csv("data/base_noshows.csv", index=False)
-
-
-# # Primeira tentativa: truncar os floats para int a bruta
-# for key in dataset_sample.keys():
-# 	dataset_sample[key] = dataset_sample[key].astype(int)
-
-# dataset_sample.to_csv("data/truncint_noshows.csv",index=False)
+dataset.to_csv("data/base_noshows_days.csv", index=False)
