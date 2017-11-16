4c646c229979509f063dada6dacfef54271d4ce5
Renato <renatocardoso1996@hotmail.com>
2017-11-16 17:48:37 +0000

    Add graphs to naive bayes

--- crabs/bayes.py
+++ crabs/bayes.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+GRAPHS_FOLDER = "bayes_graphs/"
 data = pd.read_csv("data/base_crabs.csv")
 # data = pd.read_csv("data/base_noshows.csv")
 #print(data)
@@ -22,10 +23,10 @@ target_names = np.array(["No","Yes"])
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0)
 
 # PCA part
-pca = PCA(n_components=3).fit(X)
+pca = PCA(n_components=2).fit(X)
 X_pca = pca.transform(X)
 
-pca = PCA(n_components=3).fit(X_train)
+pca = PCA(n_components=2).fit(X_train)
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 
@@ -59,83 +60,58 @@ def run_all_bayes(X, y, X_train, y_train, X_test, y_test, option):
     gnb.fit(X,y)
     print("Cross-Validation (10-fold) score: %f" % (cross_val_score(gnb, X, y, cv=10).mean()))
 
+def draw_learning_curve(X, y, X_pca, filename):
+    clf = GaussianNB()
+    train_sizes,train_scores, test_scores = learning_curve(
+        clf, X, y, cv=10, n_jobs=8)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
 
-print("\n================= Non-PCA executions ========================")
-print("\nGaussinan: \n")
-run_all_bayes(X, y, X_train, y_train, X_test, y_test, "GaussianNB")
-# print("\nMultinomial: \n")
-# run_all_bayes(X, y, X_train, y_train, X_test, y_test, "MultinomialNB")
-# print("\nBernoulli: \n")
-# run_all_bayes(X, y, X_train, y_train, X_test, y_test, "BernoulliNB")
-print("===============================================================")
+    clf_pca = GaussianNB()
+    train_sizes_pca,train_scores_pca, test_scores_pca = learning_curve(
+        clf, X_pca, y, cv=10, n_jobs=8)
+    train_scores_mean_pca = np.mean(train_scores_pca, axis=1)
+    train_scores_std_pca = np.std(train_scores_pca, axis=1)
+    test_scores_mean_pca = np.mean(test_scores_pca, axis=1)
+    test_scores_std_pca = np.std(test_scores_pca, axis=1)
 
-print("\n================= With-PCA executions =======================")
-print("\nGaussinan: \n")
-run_all_bayes(X_pca, y, X_train_pca,y_train,X_test_pca,y_test, "GaussianNB")
+    f = plt.figure()
+    plt.title("Learning Curve Naive Bayes - " + filename)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.gca().set_ylim([0.46,1.06])
+    plt.grid()
 
-# Doens't work
-# print("\nMultinomial: \n")
-# run_all_bayes(X_pca, y, X_train_pca,y_train,X_test_pca,y_test, "MultinomialNB")
+    plt.plot(train_sizes, train_scores_mean, '.-', color="r",
+             label="Training score Non-PCA")
+    plt.plot(train_sizes_pca, train_scores_mean_pca, '.-', color="b",
+             label="Training score with PCA")
+    plt.plot(train_sizes, test_scores_mean, '.-', color="g",
+             label="Cross-validation score Non-PCA")
+    plt.plot(train_sizes_pca, test_scores_mean_pca, '.-', color="y",
+             label="Cross-validation score with PCA")
 
-# print("\nBernoulli: \n")
-# run_all_bayes(X_pca, y, X_train_pca,y_train,X_test_pca,y_test, "BernoulliNB")
-print("===============================================================")
+    plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1))   
+    #plt.show()
+    f.savefig(GRAPHS_FOLDER+"lc_bayes_"+filename+".png",bbox_inches="tight")
+    f.savefig(GRAPHS_FOLDER+"lc_bayes_"+filename+".pdf",bbox_inches="tight")
 
-
-
-plt.figure()
-plt.title("Learning Curve Bayes")
-plt.xlabel("Training examples")
-plt.ylabel("Score")
-
-clf = GaussianNB()
-train_sizes,train_scores, test_scores = learning_curve(
-    clf, X, y, cv=10, n_jobs=1)
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
-
-plt.grid()
-
-plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                 train_scores_mean + train_scores_std, alpha=0.1,
-                 color="r")
-plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                 test_scores_mean + test_scores_std, alpha=0.1, color="g")
-plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-         label="Training score")
-plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-         label="Cross-validation score")
-
-plt.legend(loc="best")
-plt.show()
+def run_non_pca_knn():  
+    print("\n================= Basic Non-PCA =============================")
+    run_all_bayes(X, y, X_train, y_train, X_test, y_test, "GaussianNB")
+    print("===============================================================")
 
 
+def run_pca_knn():
+    print("\n================ Basic PCA executions =======================")
+    run_all_bayes(X_pca, y, X_train_pca,y_train,X_test_pca,y_test,"GaussianNB")
+    print("===============================================================")
 
-clf = GaussianNB()
-train_sizes,train_scores, test_scores = learning_curve(
-    clf, X_pca, y, cv=10, n_jobs=1)
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
+def draw_all_learning_curves():
+    draw_learning_curve(X,y,X_pca,"default")
 
-plt.figure()
-plt.title("Learning Curve Bayes + PCA")
-plt.xlabel("Training examples")
-plt.ylabel("Score")
-plt.grid()
-
-plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                 train_scores_mean + train_scores_std, alpha=0.1,
-                 color="r")
-plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                 test_scores_mean + test_scores_std, alpha=0.1, color="g")
-plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-         label="Training score")
-plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-         label="Cross-validation score")
-
-plt.legend(loc="best")
-plt.show()
+run_non_pca_knn()
+run_pca_knn()
+draw_all_learning_curves()
---
+++ noshows/bayes.py
@@ -0,0 +1,216 @@
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.under_sampling import ClusterCentroids
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+GRAPHS_FOLDER = "bayes_graphs/"
+data = pd.read_csv("data/base__dayofyear_noshows.csv")
+X = np.array(data.drop("class",axis=1))
+y = np.array(data["class"])
+target_names = np.array(["No","Yes"])
+
+# split dataset into training/test portions
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0, stratify=y)
+
+# PCA part
+pca = PCA(n_components=3).fit(X)
+X_pca = pca.transform(X)
+
+pca = PCA(n_components=3).fit(X_train)
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+# Over-sampling techniques
+sm = SMOTE(random_state=1)
+X_sm, y_sm = sm.fit_sample(X,y)
+X_train_sm, y_train_sm = sm.fit_sample(X_train,y_train)
+
+pca = PCA(n_components=3).fit(X_sm)
+X_sm_pca = pca.transform(X_sm)
+
+pca = PCA(n_components=3).fit(X_train_sm)
+X_train_sm_pca = pca.transform(X_train_sm)
+X_test_sm_pca = pca.transform(X_test)
+
+
+ada = ADASYN(random_state=2)
+X_ada, y_ada = ada.fit_sample(X,y)
+X_train_ada, y_train_ada = ada.fit_sample(X_train,y_train)
+
+pca = PCA(n_components=3).fit(X_ada)
+X_ada_pca = pca.transform(X_ada)
+
+pca = PCA(n_components=3).fit(X_train_ada)
+X_train_ada_pca = pca.transform(X_train_ada)
+X_test_ada_pca = pca.transform(X_test)
+
+
+
+# using n_jobs to try and paralelize computation when possible!
+
+def run_all_knn(X, y, X_train, y_train, X_test, y_test):
+    clf = GaussianNB()
+    clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    print("\nNaive Bayes classifier")
+    print(classification_report(y_test,y_pred,target_names=target_names))
+    tn, fp, fn, tp = confusion_matrix(y_test,y_pred, labels=range(2)).ravel()
+    print("TN: %d \tFP: %d \nFN: %d \tTP: %d" % (tn, fp, fn, tp))
+    print("Accuracy score: %f" % (accuracy_score(y_test,y_pred)))
+    print("ROC auc score: %f" % (roc_auc_score(y_test,y_pred)))
+    clf = GaussianNB()
+    clf.fit(X,y)
+    print("Cross-Validation (10-fold) score: %f" % (cross_val_score(clf, X, y, cv=10).mean()))
+
+def pca_components(X, y, X_train,y_train,X_test,y_test):
+    for n in range(1,11):
+        sm = ADASYN(random_state=2)
+        X_sm, y_sm = sm.fit_sample(X,y)
+        X_train_sm, y_train_sm = sm.fit_sample(X_train,y_train)
+
+        pca_test = PCA(n_components=n).fit(X_sm)
+        X_sm_pca_test = pca_test.transform(X_sm)
+
+        pca_test = PCA(n_components=n).fit(X_train_sm)
+        X_train_sm_pca_test = pca_test.transform(X_train_sm)
+        X_test_sm_pca_test = pca_test.transform(X_test)
+
+        # pca_test = PCA(n_components=n).fit(X)
+        # X_pca_test = pca_test.transform(X)
+
+        # pca_test = PCA(n_components=n).fit(X_train)
+        # X_train_pca_test = pca_test.transform(X_train)
+        # X_test_pca_test = pca_test.transform(X_test)
+
+        clf = GaussianNB()
+        clf.fit(X_train_sm_pca_test,y_train_sm)
+        y_pred = clf.predict(X_test_sm_pca_test)
+        print("Accuracy score for %d components: %f" % (n , (accuracy_score(y_test,y_pred))))
+
+def return_metric_vectors(metric, k,X_train, y_train, X_test, y_test, X_train_pca, X_test_pca):
+    metrics_functions = {
+        "roc_auc" : roc_auc_score,
+        "accuracy" : accuracy_score,
+        "precision" : precision_score,
+        "recall" : recall_score 
+    }
+    metric_score = metrics_functions[metric]
+    non_pca_metrics, with_pca_metrics = [], []
+    clf = GaussianNB()
+    clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    non_pca_metrics.append(metric_score(y_test,y_pred))
+
+    clf = GaussianNB()
+    clf.fit(X_train_pca,y_train)
+    y_pred = clf.predict(X_test_pca)
+    with_pca_metrics.append(metric_score(y_test,y_pred))
+
+    return [non_pca_metrics,with_pca_metrics]
+
+
+metrics_titles = {
+    "roc_auc" : "AUC ROC score",
+    "accuracy" : "Accuracy score",
+    "precision" : "Precision score",
+    "recall" : "Recall score"   
+}
+
+
+def draw_learning_curve(X, y, X_pca, filename):
+    clf = GaussianNB()
+    train_sizes,train_scores, test_scores = learning_curve(
+        clf, X, y, cv=10, n_jobs=8)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    clf_pca = GaussianNB()
+    train_sizes_pca,train_scores_pca, test_scores_pca = learning_curve(
+        clf, X_pca, y, cv=10, n_jobs=8)
+    train_scores_mean_pca = np.mean(train_scores_pca, axis=1)
+    train_scores_std_pca = np.std(train_scores_pca, axis=1)
+    test_scores_mean_pca = np.mean(test_scores_pca, axis=1)
+    test_scores_std_pca = np.std(test_scores_pca, axis=1)
+
+    f = plt.figure()
+    plt.title("Learning Curve Naive Bayes - " + filename)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.gca().set_ylim([0.46,0.86])
+    plt.grid()
+
+    plt.plot(train_sizes, train_scores_mean, '.-', color="r",
+             label="Training score Non-PCA")
+    plt.plot(train_sizes_pca, train_scores_mean_pca, '.-', color="b",
+             label="Training score with PCA")
+    plt.plot(train_sizes, test_scores_mean, '.-', color="g",
+             label="Cross-validation score Non-PCA")
+    plt.plot(train_sizes_pca, test_scores_mean_pca, '.-', color="y",
+             label="Cross-validation score with PCA")
+
+    plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1))   
+    #plt.show()
+    f.savefig(GRAPHS_FOLDER+"lc_bayes_"+filename+".png",bbox_inches="tight")
+    f.savefig(GRAPHS_FOLDER+"lc_bayes_"+filename+".pdf",bbox_inches="tight")
+
+def run_non_pca_knn():  
+    print("\n================= Basic Non-PCA =============================")
+    run_all_knn(X, y, X_train, y_train, X_test, y_test)
+    print("===============================================================")
+
+    print("\n================= (OS) SMOTE Non-PCA ========================")
+    run_all_knn(X, y, X_train_sm, y_train_sm, X_test, y_test)
+    print("===============================================================")
+
+    print("\n================= (OS) ADASYN Non-PCA =======================")
+    run_all_knn(X, y, X_train_ada, y_train_ada, X_test, y_test)
+    print("===============================================================")
+
+
+def run_pca_knn():
+    print("\n================ Basic PCA executions =======================")
+    run_all_knn(X_pca, y, X_train_pca,y_train,X_test_pca,y_test)
+    print("===============================================================")
+
+    print("\n================= (OS) SMOTE PCA ============================")
+    run_all_knn(X_sm_pca, y_sm, X_train_sm_pca,y_train_sm,X_test_sm_pca,y_test)
+    print("===============================================================")
+
+    print("\n================= (OS) ADASYN PCA ===========================")
+    run_all_knn(X_ada_pca, y_ada, X_train_ada_pca,y_train_ada,X_test_ada_pca,y_test)
+    print("===============================================================")
+
+def draw_all_learning_curves():
+    draw_learning_curve(X,y,X_pca,"default")
+    draw_learning_curve(X_sm,y_sm,X_sm_pca, "SMOTE")
+    draw_learning_curve(X_ada,y_ada, X_ada_pca, "ADASYN")
+
+# def draw_all_roc_graphs(k):
+#     draw_single_metric_graph("roc_auc", k, X_train, y_train, X_test, y_test,  X_train_pca, X_test_pca, "default", y_lim=[0.49,0.62])
+#     smote_roc_vectors = return_metric_vectors("roc_auc",k, X_train_sm, y_train_sm, X_test, y_test,  X_train_sm_pca, X_test_sm_pca)
+#     adasyn_roc_vectors = return_metric_vectors("roc_auc",k, X_train_ada, y_train_ada, X_test, y_test,  X_train_ada_pca, X_test_ada_pca)
+#     draw_oversampled_single_metric_graph("roc_auc",smote_roc_vectors,adasyn_roc_vectors, y_lim=[0.49,0.62])
+
+# def draw_all_accuracy_graphs(k):
+#     draw_single_metric_graph("accuracy", k, X_train, y_train, X_test, y_test,  X_train_pca, X_test_pca, "default")
+#     smote_accuracy_vectors = return_metric_vectors("accuracy", k, X_train_sm, y_train_sm, X_test, y_test,  X_train_sm_pca, X_test_sm_pca)
+#     adasyn_accuracy_vectors = return_metric_vectors("accuracy", k, X_train_ada, y_train_ada, X_test, y_test,  X_train_ada_pca, X_test_ada_pca)
+#     draw_oversampled_single_metric_graph("accuracy",smote_accuracy_vectors,adasyn_accuracy_vectors)
+
+# def draw_all_precisionrecall_graphs(k):
+#     draw_precisionrecall_graph(k, X_train, y_train, X_test, y_test,  X_train_pca, X_test_pca, "default")
+
+run_non_pca_knn()
+run_pca_knn()
+draw_all_learning_curves()
+pca_components(X, y, X_train,y_train,X_test,y_test)
